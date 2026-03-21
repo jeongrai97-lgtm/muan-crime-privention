52871095d957ee6b4ebe0b6c4810971890aa6e20
jeongrai97-lgtm <jeongrai97@gmail.com>
2026-03-21 21:30:23 +0900

    Update server.js

--- server.js
+++ server.js
@@ -356,6 +356,13 @@ app.post('/admin/posts/:id/edit', requireAdmin, (req, res) => {
     const post = db.prepare('SELECT * FROM posts WHERE id = ?').get(req.params.id);
     if (!post) return res.redirect('/admin');
 
+const isSuperAdmin = req.session.adminRole === 'superadmin';
+const isAuthor = post.author_id && Number(post.author_id) === Number(req.session.adminId);
+
+if (!isSuperAdmin && !isAuthor) {
+  return res.status(403).send('본인이 작성한 게시글만 수정할 수 있습니다.');
+}
+    
     if (err) {
       return res.status(400).render('edit', {
         post,
